00b7d2dc740fe1ea3b4ce39041b77e749376d0bb
billtthutao <billtt@163.com>
2018-07-06 15:13:07 +0800

    Add broadcast

--- server/server.js
+++ server/server.js
@@ -13,6 +13,27 @@ var io = socketIo(server);
 
 io.on('connection',(socket) => {
   console.log('New user connected');
+  
+  //welcome message to who login
+  socket.emit('newMessage',{from:'Admin',
+                            text:'Welcome to the chat app!',
+                            createdAt:new Date().getTime()
+                           }
+             );
+  //broadcast message to everyone except the one who login
+  socket.broadcast.emit('newMessage',{from:'Admin',
+                                      text:'New user joined!',
+                                      createdAt:new Date().getTime()
+                                     }
+                       );
+
+  socket.on('createMessage', (message) => {
+    console.log('createmessage',message);
+    io.emit('newMessage',{from:message.from,
+                          text:message.text,
+                          createdAt:new Date().getTime()
+                         });
+  });
   socket.on('disconnect', () => {
     console.log('User was disconnected from server');
   });
