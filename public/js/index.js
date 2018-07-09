834149ca33c37153f6ab130002b61d1c574f76d8
billtthutao <billtt@163.com>
2018-07-09 23:09:28 +0800

    Add style

--- public/js/index.js
+++ public/js/index.js
@@ -32,10 +32,13 @@
 jQuery('#message-form').on('submit',function(e){
   e.preventDefault();
 
+  var messageTextBox = jQuery('[name=message]');
+
   socket.emit('createMessage',
-             {from:'User',text:jQuery('[name=message]').val()},
+             {from:'User',text:messageTextBox.val()},
   //add callback function for server socket.on to invoke
              function (data) {
+               messageTextBox.val('');
              }
   );
 });
@@ -48,14 +51,17 @@ locationButton.on('click',
       return alert('Geolocation is not supported by your browser');
     }
     //more to add
+    locationButton.attr('disabled','disabled').text('Sending location...');
     navigator.geolocation.getCurrentPosition(
       function(position){
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage',
                     {latitude:position.coords.latitude,
                      longitude:position.coords.longitude 
                     });
       },
       function(){
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location');
     });
   });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const {generateMessage} = require('./message.js');
+const {generateMessage,generateLocationMessage} = require('./message.js');
 
 describe('generateMessage', () => {
   it('should generate message correctly', () => {
@@ -10,6 +10,21 @@ describe('generateMessage', () => {
     expect(message).toExist();
     expect(message.from).toBe(from);
     expect(message.text).toBe(text);
+    expect(message.createdAt).toBeA('number');
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate location message correctly', () => {
+    var from = 'billhu';
+    var latitude = 1;
+    var longitude = 1;
+
+    var message = generateLocationMessage(from,latitude,longitude);
+
+    expect(message).toExist();
+    expect(message.from).toBe(from);
+    expect(message.url).toBe('https://www.google.com/maps?q=1,1');
     expect(message.createdAt).toBeA('number');
   });
 });
