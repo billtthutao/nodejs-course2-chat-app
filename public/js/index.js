921eed78498ed71c7fca67249b57ec34e1f8b586
billtthutao <billtt@163.com>
2018-07-09 12:09:09 +0800

    Add jQuery

--- public/js/index.js
+++ public/js/index.js
@@ -6,9 +6,25 @@
 
  socket.on('newMessage',(message) => {
    console.log('newMessage',message);
+   //create DOM element
+   var li = jQuery('<li></li>');
+   
+   li.text(`${message.from}: ${message.text}`);
+
+   jQuery('#messages').append(li);
  });
 
  socket.on('disconnect', () => {
    console.log('DIsconect from server');
  });
 
+jQuery('#message-form').on('submit',function(e){
+  e.preventDefault();
+
+  socket.emit('createMessage',
+             {from:'User',text:jQuery('[name=message]').val()},
+  //add callback function for server socket.on to invoke
+             function (data) {
+             }
+  );
+});
